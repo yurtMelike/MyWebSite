fbd1941a21685a5ee6d30a7e4c888e9ceb2abd9c
yurtMelike <66638920+yurtMelike@users.noreply.github.com>
2020-06-25 21:43:17 +0300

    Design and pages added
    Tasarım eklendi. Sayfalar oluşturuldu.

--- MyWebSite/MyWebSite/Controllers/HomeController.cs
+++ MyWebSite/MyWebSite/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MyWebSite.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hakkımızda Sayfası";
 
             return View();
         }
