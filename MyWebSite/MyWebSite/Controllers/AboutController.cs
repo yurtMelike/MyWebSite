61d86a2154e3ca17d1c5218439ebac3a2f370ac1
yurtMelike <66638920+yurtMelike@users.noreply.github.com>
2020-06-28 13:15:45 +0300

    upd About
    Hakımızda sayfası güncellendi. Veri tabanından veriler sayfaya çekildi.

---
+++ MyWebSite/MyWebSite/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyWebSite.Models.Classes;
+
+namespace MyWebSite.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var aboutus = context.Abouts.ToList();
+            return View(aboutus);
+        }
+    }
+}
--- MyWebSite/MyWebSite/Models/Classes/Context.cs
+++ MyWebSite/MyWebSite/Models/Classes/Context.cs
@@ -9,11 +9,11 @@ namespace MyWebSite.Models.Classes
 {
     public class Context : DbContext
     {
-        DbSet<About> Abouts { get; set; }
-        DbSet<Address> Adresses { get; set; }
-        DbSet<Admin> Admins { get; set; }
-        DbSet<Blog> Blogs { get; set; }
-        DbSet<Comment> Comments { get; set; }
-        DbSet<Contact> Contacts { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Address> Adresses { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
