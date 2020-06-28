175208c48305486fa32dcb6107fd595c08d26879
yurtMelike <66638920+yurtMelike@users.noreply.github.com>
2020-06-28 19:26:52 +0300

    upd Blog
    Blog sayfası içeriği veri tabanından çekildi. Blog içeriği Id'sine göre oluşturulan detay sayfasında görüntülendi.

---
+++ MyWebSite/MyWebSite/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyWebSite.Models.Classes;
+
+namespace MyWebSite.Controllers
+{
+    public class BlogController : Controller
+    {
+        Context context = new Context();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            var blog = context.Blogs.ToList();
+            return View(blog);
+        }
+        public ActionResult BlogDetail(int id)
+        {
+            var blogFind = context.Blogs.Where(x => x.Id == id).ToList();
+            return View(blogFind);
+        }
+    }
+}
